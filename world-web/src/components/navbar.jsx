e97003e80b9f9cc7e4d6eba893508d9b86ffd6ae
Jay Rosato <rosato.jay@gmail.com>
2025-04-13 10:19:32 +0200

    Added navbar
    This commit adds a navbar that the user can use to navigate the page (still WIP). This file
    was previously named 'header', but was renamed to navbar. It also added an accomanying
    navbar.modules.css style element. Alterations were made to app.jsx to display the navbar on the homepage.

---
+++ world-web/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+export default function Header(){
+    return(
+        <div className="header">
+            <p>Icon would go here</p>
+            <h1>Epic World Here!</h1>
+            <button className="navButton">nav</button>
+            <button className="navButton">nav</button>
+            <button className="navButton">nav</button>
+        </div>
+    )
+}
