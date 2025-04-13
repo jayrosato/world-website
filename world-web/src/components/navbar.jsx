a1b8c448ce8cc8953521a639e20ac4d257a9b438
Jay Rosato <rosato.jay@gmail.com>
2025-04-13 12:20:32 +0200

    Edited homepage content and style.
    Made some further edits to the homepage (App.jsx) and its styles (app.module.css).
    Also made some minor edits to the stylings for the navbar. Still building on it, the next few
    commits will likely be more updates to these files.

--- world-web/src/components/navbar.jsx
+++ world-web/src/components/navbar.jsx
@@ -2,15 +2,17 @@ import styles from '../styles/navbar.module.css'
 
 export default function Navbar(){
     return(
-        <div className={styles.header}>
+        <nav className={styles.header}>
             <div className={styles.branding}>
                 <a href='http://localhost:5173'><img className={styles.navbarIcon} src='../public/mfi_sethianism.png' alt='temp logo' /></a>
                 <a href='http://localhost:5173' className={styles.navbarName}><h1>Epic World Here!</h1></a>
             </div>
+            <div className={styles.links}>
                 <a href='http://localhost:5173/faiths'> Realms </a>
                 <a href='http://localhost:5173/faiths'> Faiths </a>
                 <a href='http://localhost:5173/faiths'> Leaders </a>
                 <a href='http://localhost:5173/faiths'> Events </a>
-        </div>
+            </div>
+        </nav>
     )
 }
