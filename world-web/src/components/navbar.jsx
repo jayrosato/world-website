913ca547cc0aff439e2a2a0ee8438162511f8e96
Jay Rosato <rosato.jay@gmail.com>
2025-04-13 10:22:19 +0200

    Edited file structure in front end.
    Deleted header.jsx and header.css as these files have been replaced by navbar.jsx and
    navbar.module.css. Also added several files to the repo that may not have been there before.

--- world-web/src/components/navbar.jsx
+++ world-web/src/components/navbar.jsx
@@ -1,12 +1,16 @@
-import './header.css'
-export default function Header(){
+import styles from '../styles/navbar.module.css'
+
+export default function Navbar(){
     return(
-        <div className="header">
-            <p>Icon would go here</p>
-            <h1>Epic World Here!</h1>
-            <button className="navButton">nav</button>
-            <button className="navButton">nav</button>
-            <button className="navButton">nav</button>
+        <div className={styles.header}>
+            <div className={styles.branding}>
+                <a href='http://localhost:5173'><img className={styles.navbarIcon} src='../public/mfi_sethianism.png' alt='temp logo' /></a>
+                <a href='http://localhost:5173' className={styles.navbarName}><h1>Epic World Here!</h1></a>
+            </div>
+                <a href='http://localhost:5173/faiths'> Realms </a>
+                <a href='http://localhost:5173/faiths'> Faiths </a>
+                <a href='http://localhost:5173/faiths'> Leaders </a>
+                <a href='http://localhost:5173/faiths'> Events </a>
         </div>
     )
 }
